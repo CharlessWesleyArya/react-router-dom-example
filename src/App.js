0492166174420ffc9bc478660b8e713c0553bc2d
Charless Wesley Arya <wesleyarya40@gmail.com>
2022-08-25 23:31:19 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Books } from "./Books";
+import { Home } from "./Home";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello </h1>
+      <Books />
+      <Home />
     </div>
   );
 }
